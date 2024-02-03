4db3c24784ebd1d4b3a3467fd02846ea3c7a0b13
Timi <timilehinoladapo0@gmail.com>
2024-02-03 21:02:03 +0100

    fit: please build

--- sanity/sanity.client.ts
+++ sanity/sanity.client.ts
@@ -1,11 +1,11 @@
-import {createClient, type ClientConfig} from '@sanity/client'
+import { createClient, type ClientConfig } from "@sanity/client";
 
 const config: ClientConfig = {
-    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-    dataset: process.env.NEXT_PUBLIC_SANITY_DATASET, // or the name you chose in step 1
-    useCdn: false, // set to `false` to bypass the edge cache
-    apiVersion: process.env.NEXT_PUBLIC_SANITY_API_TOKEN, // use current date (YYYY-MM-DD) to target the latest API version
-}
+	projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+	dataset: process.env.NEXT_PUBLIC_SANITY_DATASET, // or the name you chose in step 1
+	useCdn: false, // set to `false` to bypass the edge cache
+	apiVersion: process.env.NEXT_PUBLIC_SANITY_API_TOKEN, // use current date (YYYY-MM-DD) to target the latest API version
+};
 const client = createClient(config);
 
 export default client;
